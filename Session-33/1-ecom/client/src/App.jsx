753e998c7e71bf282e13d50990b28c53a46a2553
Anton Francis Jeejo <antonfrancisjeejo13@gmail.com>
2025-07-07 12:08:57 +0530

    Session 33 done

---
+++ Session-33/1-ecom/client/src/App.jsx
@@ -0,0 +1,43 @@
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router";
+import Home from "./pages/Home/Home";
+import Verify from "./pages/Verify/Verify";
+import Products from "./pages/Products/Products";
+import UserRoute from "./components/UserRoute";
+import ShopRoute from "./components/ShopRoute";
+import Navbar from "./components/Navbar/Navbar";
+import { useDispatch, useSelector } from "react-redux";
+import { useEffect } from "react";
+import { handleLogin } from "./slices/user";
+import ShopDashboard from "./pages/ShopDashboard/ShopDashboard";
+
+const App = () => {
+  const dispatch = useDispatch();
+
+  const user = useSelector((state) => state.userInfo.user);
+
+  useEffect(() => {
+    const token = localStorage.getItem("token");
+    if (!user && token) {
+      dispatch(handleLogin(token));
+    }
+  }, [user]);
+
+  return (
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/user/verify/:token" element={<Verify />} />
+        <Route element={<UserRoute />}>
+          <Route path="/products" element={<Products />} />
+        </Route>
+        <Route element={<ShopRoute />}>
+          <Route path="/shop-dashboard" element={<ShopDashboard />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
+};
+
+export default App;
---
+++ Session-33/1-ecom/server/routes/product.js
@@ -0,0 +1,67 @@
+const router = require("express").Router();
+const allowedRoles = require("../middlewares/permissions");
+const verifyToken = require("../middlewares/verify");
+const Product = require("../models/Product");
+const User = require("../models/User");
+const Shop = require("../models/Shop");
+
+router.get("/", (req, res) => {
+  res.send("Product route is working");
+});
+
+router.post("/create", verifyToken, allowedRoles([3]), async (req, res) => {
+  try {
+    const shop = await Shop.findOne({ user: req.userId });
+    const product = await Product.create({
+      title: req.body.title,
+      images: req.body.images,
+      description: req.body.desc,
+      shop: shop._id,
+      rating: req.body.rating,
+      price: req.body.price,
+      quantity: req.body.quantity,
+      category: req.body.category,
+    });
+
+    res.json({ msg: "Product created successfully" });
+  } catch (error) {
+    res.json({ error });
+  }
+});
+
+router.get("/all", verifyToken, async (req, res) => {
+  try {
+    const products = await Product.find();
+    res.json({ products });
+  } catch (error) {
+    res.json({ error });
+  }
+});
+
+router.get("/:id", verifyToken, async (req, res) => {
+  try {
+    const product = await Product.findById(req.params.id);
+    res.json({ product });
+  } catch (error) {
+    res.json({ error });
+  }
+});
+
+router.post("/:id/add-review", verifyToken, async (req, res) => {
+  try {
+    console.log(req.userId);
+    const user = await User.findById(req.userId);
+    const review = {
+      name: user.name,
+      comment: req.body.review,
+    };
+    await Product.findByIdAndUpdate(req.params.id, {
+      $push: { reviews: review },
+    });
+    res.json({ msg: "Added successfully" });
+  } catch (error) {
+    res.json({ error });
+  }
+});
+
+module.exports = router;
